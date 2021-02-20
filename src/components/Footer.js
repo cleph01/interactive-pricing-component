4bd9ebe62fda5bd4823d1c80ec2a3ce1efa5c39b
Charlie <charles.j.montoya@gmail.com>
2021-02-20 17:07:24 -0500

    Footer component added

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            Unlimited websites
+            100% data ownership
+            Email reports
+
+            Start my trial
+        </div>
+    )
+}
+
+export default Footer
