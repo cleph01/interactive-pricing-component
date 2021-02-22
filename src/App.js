16798c2ef991fbb9db2ca5b2004cd6cd14eaaf3c
Charlie <charles.j.montoya@gmail.com>
2021-02-22 09:56:20 -0500

    Toggle element added and component styled

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.scss';
 
 import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,23 +12,7 @@ function App() {
       
         <Header />
         <Main />
-        <Footer />
-      
-      
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     </>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
+import Billing from './Billing'
+
+import Footer from './Footer'
+
 import '../SASS/main.scss'
 
 
 function Main() {
     return (
         <div className="pricing-component">
-            <div>
+            <div className="pricing-header">
                 Pageviews
                 $ /month
             </div>
+            
             <input className="slider" type="range" />
+
+            <Billing />
+
+            <Footer />
         </div>
     )
 }
